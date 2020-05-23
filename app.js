be12492e548a19c3b2a4fcbb329046869e9ec8dc
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-05-23 16:07:47 +0100

    Implement Initial Fuel Cost Calculation

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-window.addEventListener("load", function () {
-    let forms = this.document.getElementsByClassName("needs-validation");
-    // There's only one form on this page so, get first element in array.
-    let form = forms[0];
-    let validation = form.addEventListener("submit", function (event) {
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        form.classList.add("was-validated");
-    })
-}, false);
-
+window.addEventListener(
+    "load",
+    function () {
+        let forms = this.document.getElementsByClassName("needs-validation");
+        // There's only one form on this page so, get first element in array.
+        let form = forms[0];
+        let validation = form.addEventListener("submit", function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+            let distance = 200;
+            let mpg = document.getElementById("vehicleMPG").value;
+            let fuelCost = document.getElementById("costOfFuel").value;
+            document.getElementById("calcResult").value = calculateFuelCost(
+                distance,
+                mpg,
+                fuelCost
+            );
+        });
+    },
+    false
+);
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     alert("Doc loaded")
 // })
 
-const mainHeading = document.querySelector("h1")
-mainHeading.addEventListener("click", calculateFuelCost);
+// const mainHeading = document.querySelector("h1")
+// mainHeading.addEventListener("click", calculateFuelCost);
 
-function calculateFuelCost() {
-    mainHeading.textContent = "£20"
+function calculateFuelCost(distance, mpg, fuelCost) {
+    // Imperial gallons
+    const litresPerGallon = 4.54609;
+    let gallonsForJourney = distance / mpg;
+    let litresForJourney = gallonsForJourney * litresPerGallon;
+    let costOfJourney = litresForJourney * fuelCost;
+    return costOfJourney;
 }
 
+// document.getElementById("calcResult").textContent = calculateFuelCost();
 
 function distanceCallback(response, status) {
     if (status !== "OK") {
@@ -36,13 +53,16 @@ function distanceCallback(response, status) {
 }
 
 function initMap(origin, destination) {
-    origin = "WD19 5AX"
-    destination = "BA14 7WR"
-    var service = new google.maps.DistanceMatrixService;
-    var test = service.getDistanceMatrix({
-        origins: [origin],
-        destinations: [destination],
-        travelMode: "DRIVING",
-        unitSystem: google.maps.UnitSystem.IMPERIAL
-    }, distanceCallback);
+    origin = "WD19 5AX";
+    destination = "BA14 7WR";
+    var service = new google.maps.DistanceMatrixService();
+    var test = service.getDistanceMatrix(
+        {
+            origins: [origin],
+            destinations: [destination],
+            travelMode: "DRIVING",
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+        },
+        distanceCallback
+    );
 }
